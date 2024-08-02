6a2acc75765d0cfc9f0945c1c59cde53c0b81cb5
Laura Pais <pais.laura@gmail.com>
2024-08-02 10:06:11 +0100

    ++

--- script.js
+++ script.js
@@ -7,20 +7,35 @@ videoSlider.addEventListener('input', function() {
     multimediaVideo1.style.opacity = opacity;
 });
 
-
 // Poem
 const poemSlider = document.getElementById('poemSlider');
 const poem1 = document.getElementById('poem1');
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-const gainNode = audioContext.createGain();
-const source = audioContext.createMediaElementSource(poem1);
-source.connect(gainNode);
-gainNode.connect(audioContext.destination);
+const gainNodePoem = audioContext.createGain();
+const sourcePoem = audioContext.createMediaElementSource(poem1);
+sourcePoem.connect(gainNodePoem);
+gainNodePoem.connect(audioContext.destination);
 
-gainNode.gain.value = poemSlider.value / 100;
+gainNodePoem.gain.value = poemSlider.value / 100;
 
 poemSlider.addEventListener('input', function() {
     const gainValue = poemSlider.value / 100;
-    gainNode.gain.value = gainValue;
+    gainNodePoem.gain.value = gainValue;
+});
+
+// Music
+const musicSlider = document.getElementById('musicSlider');
+const music1 = document.getElementById('music1');
+
+const gainNodeMusic = audioContext.createGain();
+const sourceMusic = audioContext.createMediaElementSource(music1);
+sourceMusic.connect(gainNodeMusic);
+gainNodeMusic.connect(audioContext.destination);
+
+gainNodeMusic.gain.value = musicSlider.value / 100;
+
+musicSlider.addEventListener('input', function() {
+    const gainValue = musicSlider.value / 100;
+    gainNodeMusic.gain.value = gainValue;
 });

9af0b31fa01ff2b62d136e660b8b648c886a06ae
Laura Pais <pais.laura@gmail.com>
2024-09-18 09:56:34 +0100

    ++

--- script_teste.js
+++ script_teste.js
@@ -1,29 +1,16 @@
+const startButton = document.getElementById('startButton');
+const jazzAudio = document.querySelector('.track1 .jazz');
+const poesiaAudio = document.querySelector('.track1 .poesia');
+const videoElement = document.querySelector('.track1 .video');
+const legendasVideo = document.querySelector('.track1 .legendas');
 
-    const startButton = document.getElementById('startButton');
-    const jazzAudio = document.querySelector('.track1 .jazz');
-    const poesiaAudio = document.querySelector('.track1 .poesia');
-    const videoElement = document.querySelector('.track1 .video');
-    const legendasVideo = document.querySelector('.track1 .legendas');
+videoElement.style.display = legendasVideo.style.display = 'none';
 
-    let interactionEnabled = false;
-
-    videoElement.style.display = 'none';
-    legendasVideo.style.display = 'none';
-
-    startButton.addEventListener('click', function() {
-        interactionEnabled = true;
-
-        jazzAudio.play();
-        poesiaAudio.play();
-        videoElement.play();
-        legendasVideo.play();
-
-        videoElement.style.display = 'block';
-        videoElement.style.opacity = '1';
-        legendasVideo.style.display = 'block';
-        legendasVideo.style.opacity = '1';
-
-        startButton.style.display = 'none';
-    });
-
+startButton.addEventListener('click', () => {
+    [jazzAudio, poesiaAudio, videoElement, legendasVideo].forEach(el => el.play());
     
+    videoElement.style.display = legendasVideo.style.display = 'block';
+    videoElement.style.opacity = legendasVideo.style.opacity = '1';
+    
+    startButton.style.display = 'none';
+});

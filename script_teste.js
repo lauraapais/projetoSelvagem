faaf4b82b7de839efd35e27b21aeca1159424ecc
Laura Pais <pais.laura@gmail.com>
2024-09-18 11:20:25 +0100

    ++

--- script_teste.js
+++ script_teste.js
@@ -7,7 +7,6 @@ let poesiaAudio = currentTrack.querySelector('.poesia');
 let videoElement = currentTrack.querySelector('.video');
 let legendasVideo = currentTrack.querySelector('.legendas');
 
-// Escondemos os elementos de vídeo e legendas inicialmente
 videoElement.style.display = 'none';
 legendasVideo.style.display = 'none';
 
@@ -16,53 +15,45 @@ let interactionEnabled = false;
 startButton.addEventListener('click', () => {
     interactionEnabled = true;
 
-    // Inicia a reprodução da faixa inicial
-    playCurrentTrack();
+    jazzAudio.play();
+    poesiaAudio.play();
+    videoElement.play();
     
-    startButton.style.display = 'none'; // Esconde o botão de início após o clique
+    videoElement.style.display = 'block';
+    videoElement.style.opacity = '1';
+
+    startButton.style.display = 'none';
 });
 
-// Função para reproduzir a faixa atual
-function playCurrentTrack() {
-    if (interactionEnabled) { // Garante que o usuário já tenha interagido
-        jazzAudio.play();
-        poesiaAudio.play();
-        videoElement.play();
-
-        // Exibe os vídeos
-        videoElement.style.display = 'block';
-        videoElement.style.opacity = '1';
-        legendasVideo.style.display = 'block';
-    }
-}
-
-// Função para parar a faixa atual
 function stopCurrentTrack() {
     if (jazzAudio) jazzAudio.pause();
     if (poesiaAudio) poesiaAudio.pause();
     if (videoElement) videoElement.pause();
 
-    // Esconde os vídeos ao parar a faixa
     videoElement.style.display = 'none';
     legendasVideo.style.display = 'none';
 }
 
-// Função para alternar entre as faixas
 function playTrack(trackElement) {
-    stopCurrentTrack(); // Para a faixa atual antes de iniciar a nova
+    stopCurrentTrack(); 
 
     currentTrack = trackElement;
 
-    // Atualiza os elementos da nova faixa
     jazzAudio = currentTrack.querySelector('.jazz');
     poesiaAudio = currentTrack.querySelector('.poesia');
     videoElement = currentTrack.querySelector('.video');
     legendasVideo = currentTrack.querySelector('.legendas');
 
-    playCurrentTrack(); // Reproduz a nova faixa
+   
+    jazzAudio.play();
+    poesiaAudio.play();
+    videoElement.play();
+
+    videoElement.style.display = 'block';
+    videoElement.style.opacity = '1';
+    legendasVideo.style.display = 'block';
 }
 
-// Adiciona os event listeners a cada faixa
 const faixaElements = document.querySelectorAll('.faixa');
 faixaElements.forEach(faixa => {
     faixa.addEventListener('click', () => {
@@ -74,6 +65,6 @@ faixaElements.forEach(faixa => {
             return;
         }
 
-        playTrack(trackElement); // Reproduz a nova faixa quando o usuário clicar
+        playTrack(trackElement); 
     });
 });

2b550abdde6eb1cd2258c5cef8a05f4bc7adcbbb
Laura Pais <pais.laura@gmail.com>
2024-09-19 10:21:13 +0100

    ++

--- js/script_final.js
+++ js/script_final.js
@@ -223,8 +223,8 @@ divBottom.addEventListener('touchmove', (e) => {
     if (!interactionEnabled) return;
 
     handleTouchMove(e, divBottom, (opacity) => {
-        legendasVideo.style.opacity = opacity; // Inverte a opacidade
-        updateProgressBar(progressLegendas, opacity, true);
+        legendasVideo.style.opacity = opacity; // Ajusta a opacidade normalmente
+        updateProgressBar(progressLegendas, 1 - opacity, true); // Inverte o progresso da barra
     }, true);
 });
 
